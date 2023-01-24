fe95e3559d6dab5548fe1032e808449fad1f3315
Ariel Ibarra <akz.ibarra@yavirac.edu.ec>
2023-01-24 17:53:21 -0500

    Update: datosTablas/migracionDB

--- api_persona/api/models.py
+++ api_persona/api/models.py
@@ -3,8 +3,8 @@ class Tipo_persona (models.Model):
     tipo = models.CharField(max_length=80)  
 
 class Persona (models.Model):
-    nombre = models.CharField(max_length=80)
-    cedula = models.IntegerField()
+    razon_social = models.CharField(max_length=80)
+    nro_identificacion = models.IntegerField()
     fecha_nacimiento = models.DateField(auto_now=False, auto_now_add=False)
     monto_primera_compra = models.FloatField()
     tipo=models.ForeignKey(Tipo_persona,on_delete=models.PROTECT)
